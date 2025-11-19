b2e8c76e34a0b9aa5b3252c8de911465ccb68d66
KOJ5645870 <koj5695@gmail.com>
2025-11-19 10:31:09 +0900

    무기 2종 추가
    권총(Pistol), 스나이퍼(Sniper) 추가

---
+++ Assets/FPS/Scripts/Game/Shared/SniperScopeManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SniperScopeManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scope;
+
+
+    private void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
